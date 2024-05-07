769abe222f0f5b5914d6d06e8c790d5d71748f06
ChiaraPontello <chiarapontello1@gmail.com>
2024-05-07 15:17:00 +0200

    components & header.jsx

---
+++ src/assets/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from './../Images/logo.png'
+
+function Header() {
+  return (
+    <div>
+        <img src={logo} className='w-[80px] obj' />
+    </div>
+  )
+}
+
+export default Header

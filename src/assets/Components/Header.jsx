c67ba3b74708cb36671fb0eed9d67ae034d96417
ChiaraPontello <chiarapontello1@gmail.com>
2024-05-07 15:59:11 +0200

    Header & install icon React

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import logo from './../Images/logo.png'
+import { HiHome,
+  HiMagnifyingGlass,
+  HiStar,
+  HiPlayCircle,
+  HiTv } from "react-icons/hi2";
+import { HiPlus,HiDotsVertical } from "react-icons/hi";
+import HeaderItem from './HeaderItem';
 
-function Header() {
+
+  function Header() {
+    const menu=[
+        {
+            name:'HOME',
+            icon:HiHome
+        },
+        {
+            name:'SEARCH',
+            icon:HiMagnifyingGlass
+        },
+        {
+            name:'WATCH LIST',
+            icon:HiPlus
+        },
+        {
+            name:'ORIGINALS',
+            icon:HiStar
+        },
+        {
+            name:'MOVIES',
+            icon:HiPlayCircle
+        },
+        {
+            name:'SERIES',
+            icon:HiTv
+        }
+    ]
+
   return (
     <div>
-        <img src={logo} className='w-[80px] obj' />
+        <img src={logo} className='w-[80px] md:w-[115px] obj-cover' />
+        {menu.map((item)=>(
+          <HeaderItem name={item.name} Icon={item.icon}/>
+        ))}
     </div>
   )
 }
---
+++ src/assets/Components/HeaderItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeaderItem({name, Icon}) {
+  return (
+    <div>HeaderItem</div>
+  )
+}
+
+export default HeaderItem

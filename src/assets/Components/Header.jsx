5b9e500ff181576d555008f50d48881c4f8851ec
ChiaraPontello <chiarapontello1@gmail.com>
2024-05-07 17:15:41 +0200

    Toggle menu

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../Images/logo.png'
 import avatar from './../Images/avatar.jpg'
 import { HiHome,
@@ -11,6 +11,7 @@ import HeaderItem from './HeaderItem';
 
 
   function Header() {
+    const [toggle, setToggle]=useState(false);
     const menu=[
         {
             name:'HOME',
@@ -42,9 +43,25 @@ import HeaderItem from './HeaderItem';
     <div className='flex items-center justify-between p-5'>
       <div className='flex gap-8 items-center'>
         <img src={logo} className='w-[80px] md:w-[115px] obj-cover' />
+        <div className='hidden md:flex gap-8'>
         {menu.map((item)=>(
           <HeaderItem name={item.name} Icon={item.icon}/>
         ))}
+        </div>
+        <div className='flex md:hidden gap-5'>
+        {menu.map((item, index)=>index<3&&(
+          <HeaderItem name={''} Icon={item.icon}/>
+        ))}
+          <div className='md:hidden'onClick={()=>setToggle(!toggle)}>
+            <HeaderItem name={''} Icon={HiDotsVertical} />
+            {toggle? 
+            <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+              {menu.map((item, index)=>index>2&&(
+              <HeaderItem name={item.name} Icon={item.icon}/>
+              ))}
+            </div>:null}
+          </div>
+        </div>
         </div>
         <img src={avatar} className='w-[40px] rounded-full'/>
     </div>
